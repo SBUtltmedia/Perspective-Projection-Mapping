9c1a56b47be9848e188367f46a2840a6e7f3d0fa
shrn <shrn@protonmail.com>
2017-04-03 19:03:05 -0400

    Reordering array (/test)

--- test/test.js
+++ test/test.js
@@ -1,74 +1,62 @@
-// Sorry.
-// I'm terrible at javascript- this is me trying to learn it.
+// INPUT:
+var realOrderPoints = [
+    [0.2908355050629197, 0.644665000100760], // 0
+    [0.4975054795604854, 0.606452937014772], // 1
+    [0.3036731145136822, 0.365250418155803], // 2
+    [0.4976877521637637, 0.276594389044925], // 3
+    [0.7098655613684729, 0.644041915810630], // 4
+    [0.5017728817543318, 0.671290203754576], // 5
+    [0.6969444100477866, 0.363786872907236], // 6
+    [0.5016788268098611, 0.428357748114594], // 7        DEL
+]
+
+// OUTPUT:
+var vertexPoints = [
+    [0.5011, 0.6064], // A
+    [0.6334, 0.6445], // B
+    [0.6252, 0.3650], // C
+    [0.5010, 0.2765], // D
+    [0.3662, 0.6441], // E
+    [0.3744, 0.3639], // F
+    [0.4991, 0.6713], // G
+]
 
 
-reorderPoints();
+var reorderArray = calculateReorderPoints(realOrderPoints, vertexPoints);
 
 
+// To Do: apply reorderArray sequence to realOrderPoints. Generate new array with ordered values.
+function getArr(vertexPoints, reOrderArray) {
+    var orderedArr;
+    return orderedArr;
+}
 
 
-
-
-
-function reorderPoints() {
-
-    // INPUT:
-    var realOrderPoints = [
-        [0.2908355050629197, 0.644665000100760], // 0
-        [0.4975054795604854, 0.606452937014772], // 1
-        [0.3036731145136822, 0.365250418155803], // 2
-        [0.4976877521637637, 0.276594389044925], // 3
-        [0.7098655613684729, 0.644041915810630], // 4
-        [0.5017728817543318, 0.671290203754576], // 5
-        [0.6969444100477866, 0.363786872907236], // 6
-        [0.5016788268098611, 0.428357748114594], // 7        DEL
-    ]
-
-
-    // OUTPUT:
-    var vertexPoints = [
-        [0.5011, 0.6064], // A
-        [0.6334, 0.6445], // B
-        [0.6252, 0.3650], // C
-        [0.5010, 0.2765], // D
-        [0.3662, 0.6441], // E
-        [0.3744, 0.3639], // F
-        [0.4991, 0.6713], // G
-    ]
-
-
-
-    var deltaX, deltaY;
-    var mapVerts = [];
-
-    var x = vertexPoints.length;
-    var y = realOrderPoints.length;
-    // console.log(x);
-    // console.log(y);
-
-
-
-    // use forEach
-
-//     for (i=0; i<x; ++i){
-//         console.log( vertexPoints[i] );
-
-//     }
-
-
-// }
-
-
-
-
-
+function calculateReorderPoints(realOrderPoints, vertexPoints) {
+    var distanceArr = [];
+    realOrderPoints.pop()
+    realOrderPoints.forEach(function(val, index, array) {
+        var minVal = 1000;
+        var minIndex = 0;
+        vertexPoints.forEach(function(val2, index2, array2) {
+            var totalDist = Math.abs(val[0] - val2[0]) + Math.abs(val[1] - val2[1]);
+            if (totalDist < minVal) {
+                minVal = totalDist;
+                minIndex = index2;
+            }
+        });
+        console.log(minVal)
+        distanceArr.push(minIndex);
+    });
+    return distanceArr;
+}
 
 
 
 
 
 /*
-
+PROBLEM:
 // 1) Get array sequence with minimum deltas
 // 2) Remap
 // ** Note: Ensure no point gets used twice.
@@ -87,8 +75,6 @@ var realOrderPoints = [
     [0.5016788268098611, 0.428357748114594],        // 7        DEL
 ]
 
-
-
 // OUTPUT:
 var vertexPoints = [
     [0.5011, 0.6064], // A
@@ -102,6 +88,8 @@ var vertexPoints = [
 ]
 
 
+> 4 0 5 2 1 6 2
+
 
 // BASE CASE:
 0   -> E
@@ -113,12 +101,4 @@ var vertexPoints = [
 6   -> C
 7   -> _
 
-
-
 */
-
-
-
-
-
-
